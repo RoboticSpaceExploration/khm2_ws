e573ea5993fca2e46ea0e5db0a8a67ff2357cd4c
Bret Witt <bretwitt@hawaii.edu>
2022-10-01 02:22:45 -1000

    Initial commit

---
+++ build/khm2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bretwitt/khm2_ws/src/khm2_description
+# Build directory: /home/bretwitt/khm2_ws/build/khm2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/khm2_desktop/khm2_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bretwitt/khm2_ws/src/khm2_desktop/khm2_gazebo
+# Build directory: /home/bretwitt/khm2_ws/build/khm2_desktop/khm2_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

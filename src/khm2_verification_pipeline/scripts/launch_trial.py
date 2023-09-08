b4d33816edabff656769c0d18384500962cee75c
RiverMatsumoto <matsumotorjames@gmail.com>
2023-09-07 15:56:38 -1000

    analysis and rosbag pro processing improvements

--- src/khm2_verification_pipeline/scripts/launch_trial.py
+++ src/khm2_verification_pipeline/scripts/launch_trial.py
@@ -3,12 +3,25 @@ import rospy
 from geometry_msgs.msg import Twist
 import roslaunch
 
-# TODO think about "resetting the rover"
+# read trial parameters
+time = rospy.get_param('/trial_params/time', None) # s
+speed = rospy.get_param('/trial_params/speed', None) # cm/s
+angle = rospy.get_param('/trial_params/angle', None) # degrees
+if time == None:
+    rospy.set_param('/trial_params/time', 15)
+    time = 15
+if speed == None:
+    rospy.set_param('/trial_params/speed', 4)
+    speed = 6
+if angle == None:
+    rospy.set_param('/trial_params/angle', 0)
+    angle = 0
 
+file_name = f"sim_{angle}DEG"
 # launch rosbag record and close when node is done with trial
 rosbag_pkg = 'rosbag'
 rosbag_exe = 'record'
-rosbag_node = roslaunch.core.Node(rosbag_pkg, rosbag_exe, args="-o $(find khm2_verification_pipeline)/data/sim /joint_states /front_left_torque /front_right_torque /back_left_torque /back_right_torque /imu /gazebo/model_states", output="screen")
+rosbag_node = roslaunch.core.Node(rosbag_pkg, rosbag_exe, args=f"-o $(find khm2_verification_pipeline)/data/{file_name} /joint_states /front_left_torque /front_right_torque /back_left_torque /back_right_torque /imu /gazebo/model_states", output="screen")
 
 launch = roslaunch.scriptapi.ROSLaunch()
 launch.start()
@@ -21,19 +34,6 @@ def shutdown_node(event):
 
 def main():
     rospy.init_node('run_trial_node')
-    # read trial parameters
-    time = rospy.get_param('/trial_params/time', None) # s
-    speed = rospy.get_param('/trial_params/speed', None) # cm/s
-    angle = rospy.get_param('/trial_params/angle', None) # degrees
-    if time == None:
-        rospy.set_param('/trial_params/time', 15)
-        time = 15
-    if speed == None:
-        rospy.set_param('/trial_params/speed', 4)
-        speed = 6
-    if angle == None:
-        rospy.set_param('/trial_params/angle', 0)
-        angle = 0
 
     cmd_vel_pub = rospy.Publisher("/robot1_velocity_controller/cmd_vel", Twist, queue_size=10)
 
